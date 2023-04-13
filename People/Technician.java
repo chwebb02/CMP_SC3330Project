42db696d1ade27e963f7bb9a7240437cff4f7201
Patrick Gardner <patrickgardner4@gmail.com>
2023-04-13 17:45:07 -0500

    added functionality to technician class

--- People/Technician.java
+++ People/Technician.java
@@ -2,21 +2,32 @@ package People;
 
 import Utils.*;
 import java.time.LocalDate;
+import java.util.Collection;
+import java.util.Stack;
+
 import Collections.*;
 
-public class Technician extends Employee {
+
+ 
+ public class Technician extends Employee {
+   private Stack<Collections.LibraryCollection> TBR;
+
+
+    public void pushTBR(Collections.LibraryCollection item)
+    {
+        TBR.push(item);
+    }
+
+    public void pullTBR()
+    {
+        TBR.pop().returnToCollection();
+    }
+
     Technician(String name, String address, Email email, Login login, LocalDate dob, SSN ssn) {
         super(name, address, email, login, dob, ssn);
     }
-    
-    public static void  reShelveItem(Collections.LibraryCollection item ){ //takes an item to be reshelved, if the item is checked out, it is changed to be avaliable
-        //if for some reason it is avaliable, nothing happens
-        if( !item.checkOut())
-        {
-            item.returnToCollection();
-        }
-        else{System.out.println("This item is already reshelved");}
-    }
+
+
 
 
 
