127264c9f44469907028bfc559a8603b029fda47
Patrick Gardner <patrickgardner4@gmail.com>
2023-04-13 17:41:04 -0500

    added functionality to the technicial class

--- People/Technician.java
+++ People/Technician.java
@@ -2,9 +2,33 @@ package People;
 
 import Utils.*;
 import java.time.LocalDate;
+import java.util.Collection;
+import java.util.Stack;
 
-public class Technician extends Employee {
+import Collections.*;
+
+
+ 
+ public class Technician extends Employee {
+   private Stack<Collections.LibraryCollection> TBR;
+
+
+    public void pushTBR(Collections.LibraryCollection item)
+    {
+        TBR.push(item);
+    }
+
+    public void pullTBR()
+    {
+        TBR.pop().returnToCollection();
+    }
+
     Technician(String name, String address, Email email, Login login, LocalDate dob, SSN ssn) {
         super(name, address, email, login, dob, ssn);
     }
+
+
+
+
+
 }
