cdf4930c7dacde7f866ae3b15bf31604b9553cc9
Patrick Gardner <patrickgardner4@gmail.com>
2023-04-12 19:36:02 -0500

    some basic classes for the 2 employee classes

--- People/Librarian.java
+++ People/Librarian.java
@@ -2,9 +2,45 @@ package People;
 
 import Utils.*;
 import java.time.LocalDate;
+import java.util.Queue;
+import java.util.Scanner;
+import Collections.*;
 
 public class Librarian extends Employee{
     Librarian(String name, String address, Email email, Login login, LocalDate dob, SSN ssn) {
         super(name, address, email, login, dob, ssn);
+    }
+    Queue <String> returned;
+    
+    public void assistance()
+    {
+        Scanner input=new Scanner(System.in);
+        String resp;
+
+        System.out.println("Hello, welcome to the Library, what can I help you with?\n>Membership\n>Checkout an item\n>Quit");//More to be added
+        resp=input.nextLine();
+            input.nextLine();
+        while(!resp.contains("Quit"))   
+        {
+            switch (resp) {
+                case "Membership":
+                    System.out.println("You are inside the Membership case");
+                    break;
+            
+                default:
+                    System.out.println("Im sorry, that isnt a valid option, please check your spelling and try again");
+                    break;
+                case"return":
+                System.out.println("Please enter the item that you are returning");
+                returnItem(input.nextLine());
+                break;
+            }
+        }
+        System.out.println("Thank you for using the libray, come back soon :)");
+        input.close();
+    }
+    private void returnItem(String item)
+    {
+        returned.add(item);
     }
 }
--- People/Technician.java
+++ People/Technician.java
@@ -2,9 +2,22 @@ package People;
 
 import Utils.*;
 import java.time.LocalDate;
+import Collections.*;
 
 public class Technician extends Employee {
     Technician(String name, String address, Email email, Login login, LocalDate dob, SSN ssn) {
         super(name, address, email, login, dob, ssn);
     }
+    
+    public static void  reShelveItem(Collections.LibraryCollection item ){ //takes an item to be reshelved, if the item is checked out, it is changed to be avaliable
+        //if for some reason it is avaliable, nothing happens
+        if( !item.checkOut())
+        {
+            item.returnToCollection();
+        }
+        else{System.out.println("This item is already reshelved");}
+    }
+
+
+
 }
